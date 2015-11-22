ae63fee2c5ae8e016c7004b01aed59fded5ad058
MLauper <laupm4@bfh.ch>
2015-11-22 11:22:08 -0500

    Add Files

--- sample-min.lisp
+++ sample-min.lisp
@@ -1,6 +1,8 @@
-(require :cl-fuse-meta-fs)
 #+sbcl (require :sb-posix)
-(require :iterate)
+;;(require :iterate)
+;;(require :cl-fuse-meta-fs)
+
+(asdf:oos 'asdf:load-op 'cl-fuse-meta-fs)
 
 ;(eval-when (:compile-toplevel :load-toplevel :execute)
 ;  (unexport 'iterate:terminate :iterate))
@@ -8,14 +10,11 @@
 (use-package :cl-fuse-meta-fs)
 (use-package :iterate)
 
-(defvar *description* 
-    (list
-         (mk-file "README" "Hello here!")
-	    )
-     )
+(defvar *description* (list (cl-fuse-meta-fs:mk-file "README" "Hello here!")))
  
 (setf cl-fuse::*break-on-errors* t)
 
 (cl-fuse:fuse-complain "FS is ~s~%" *description*)
 
 (cl-fuse-meta-fs:run-lisp-meta-fs *description*)
+
